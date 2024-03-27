2063bd2113f281a63615d88f1e4c7f64a29eb3ea
NTM16 <164037540+NTM16@users.noreply.github.com>
2024-03-27 09:50:12 +0700

    Add files via upload

---
+++ 2703.py
@@ -0,0 +1,12 @@
+
+cyclical = pd.read_excel('nckh.xlsx', sheet_name='Consumer Cyclical')
+utilities = pd.read_excel('nckh.xlsx', sheet_name='Utilities')
+communication = pd.read_excel('nckh.xlsx', sheet_name='Communication Services')
+technology = pd.read_excel('nckh.xlsx', sheet_name='Technology')
+estate = pd.read_excel('nckh.xlsx', sheet_name='Real Estate')
+industrials = pd.read_excel('nckh.xlsx', sheet_name='Industrials')
+health = pd.read_excel('nckh.xlsx', sheet_name='Healthcare')
+financial = pd.read_excel('nckh.xlsx', sheet_name='Financial Services')
+energy = pd.read_excel('nckh.xlsx', sheet_name='Energy')
+defensive = pd.read_excel('nckh.xlsx', sheet_name='Consumer Defensive')
+materials = pd.read_excel('nckh.xlsx', sheet_name='Basic Materials')
